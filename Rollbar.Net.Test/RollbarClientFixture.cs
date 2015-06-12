fe98bec4d957882ba813eed17845b67ae498930a
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 15:13:04 -0400

    Add other rollbar client tests

--- Rollbar.Net.Test/RollbarClientFixture.cs
+++ Rollbar.Net.Test/RollbarClientFixture.cs
@@ -13,5 +13,17 @@ namespace Rollbar.Test {
         public void Client_rendered_as_dict_when_empty() {
             Assert.Equal("{}", JsonConvert.SerializeObject(_rollbarClient));
         }
+
+        [Fact]
+        public void Client_renders_arbitrary_keys_correctly() {
+            _rollbarClient["test-key"] = "test-value";
+            Assert.Equal("{\"test-key\":\"test-value\"}", JsonConvert.SerializeObject(_rollbarClient));
+        }
+
+        [Fact]
+        public void Client_renders_javascript_entry_correctly() {
+            _rollbarClient.Javascript = new RollbarJavascriptClient();
+            Assert.Equal("{\"javascript\":{}}", JsonConvert.SerializeObject(_rollbarClient));
+        }
     }
 }
