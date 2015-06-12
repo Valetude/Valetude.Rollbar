5b5f822ad83dff8c390a18de9f115087ebf883f4
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 16:28:39 -0400

    Add tests for CodeContext

---
+++ Rollbar.Net.Test/RollbarCodeContextFixture.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Rollbar.Test {
+    public class RollbarCodeContextFixture {
+        private RollbarCodeContext _rollbarCodeContext;
+
+        public RollbarCodeContextFixture() {
+            this._rollbarCodeContext = new RollbarCodeContext {
+                Pre = new[] {
+                    "public RollbarCodeContextFixture() {",
+                    "this._rollbarCodeContext = new RollbarCodeContext {",
+                },
+                Post = new[] {
+                    "};",
+                    "}",
+                }
+            };
+        }
+
+        [Fact]
+        public void Context_serializes_reasonably() {
+            var json = JsonConvert.SerializeObject(_rollbarCodeContext);
+            var jobj = JObject.Parse(json);
+            var pre = Assert.IsType<JArray>(jobj["pre"]).Select(j => j.Value<string>()).ToArray();
+            Assert.Equal(2, pre.Length);
+            Assert.Equal(_rollbarCodeContext.Pre, pre);
+            var post = Assert.IsType<JArray>(jobj["post"]).Select(j => j.Value<string>()).ToArray();
+            Assert.Equal(2, post.Length);
+            Assert.Equal(_rollbarCodeContext.Post, post);
+        }
+
+    }
+}
