b43af4de745fd7a2ed271260de620848d77171e8
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 12:43:09 -0400

    Write tests for RollbarPerson

--- Rollbar.Net.Test/RollbarPersonFixture.cs
+++ Rollbar.Net.Test/RollbarPersonFixture.cs
@@ -1,5 +1,28 @@
-﻿namespace Rollbar.Test {
-    public class RollbarPersonFixture {
+﻿using Newtonsoft.Json;
+using Xunit;
 
+namespace Rollbar.Test {
+    public class RollbarPersonFixture {
+        [Fact]
+        public void Person_id_rendered_correctly() {
+            var rp = new RollbarPerson("person_id");
+            Assert.Equal("{\"id\":\"person_id\"}", JsonConvert.SerializeObject(rp));
+        }
+
+        [Fact]
+        public void Person_username_rendered_correctly() {
+            var rp = new RollbarPerson("person_id") {
+                UserName = "chris_pfohl",
+            };
+            Assert.Equal("{\"id\":\"person_id\",\"username\":\"chris_pfohl\"}", JsonConvert.SerializeObject(rp));
+        }
+
+        [Fact]
+        public void Person_email_rendered_correctly() {
+            var rp = new RollbarPerson("person_id") {
+                Email = "chris@valetude.com",
+            };
+            Assert.Equal("{\"id\":\"person_id\",\"email\":\"chris@valetude.com\"}", JsonConvert.SerializeObject(rp));
+        }
     }
 }
