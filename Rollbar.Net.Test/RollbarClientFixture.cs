8731f187af2ac2a3f0718db80acf24738827485b
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 15:10:56 -0400

    Add RollbarClient test

---
+++ Rollbar.Net.Test/RollbarClientFixture.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Rollbar.Test {
+    public class RollbarClientFixture {
+        private readonly RollbarClient _rollbarClient;
+
+        public RollbarClientFixture() {
+            this._rollbarClient= new RollbarClient();
+        }
+
+        [Fact]
+        public void Client_rendered_as_dict_when_empty() {
+            Assert.Equal("{}", JsonConvert.SerializeObject(_rollbarClient));
+        }
+    }
+}
