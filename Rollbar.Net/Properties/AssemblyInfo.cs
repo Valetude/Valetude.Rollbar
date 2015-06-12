a797df245cb10bed2deef813b4bf5d470890a7f9
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 11:31:09 -0400

    Update Assembly so everything can actually run

--- Rollbar.Net/Properties/AssemblyInfo.cs
+++ Rollbar.Net/Properties/AssemblyInfo.cs
@@ -1,32 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Rollbar.Net")]
+#region General Infomation
+[assembly: AssemblyTitle("Rollbar")]
 [assembly: AssemblyDescription("C# Rollbar Payload, post it whenever and however it makes sense")]
 [assembly: AssemblyCompany("Valetude LLC")]
 [assembly: AssemblyProduct("Rollbar.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyCulture("en-US")]
+#endregion
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+#region Com
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("034b2c39-d11f-4285-9e1a-f423c01133cc")]
+#endregion
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
