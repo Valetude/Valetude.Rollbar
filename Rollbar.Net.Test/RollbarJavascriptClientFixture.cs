bc82fb0eacb6750dbfac183066161713b0a4fccd
Christopher Pfohl <Christopher.Pfohl@gmail.com>
2015-06-12 15:34:17 -0400

    Add very basic Javascript Client tester

---
+++ Rollbar.Net.Test/RollbarJavascriptClientFixture.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Xunit;
+
+namespace Rollbar.Test {
+    public class RollbarJavascriptClientFixture {
+        private readonly RollbarJavascriptClient _rollbarJsClient;
+
+        public RollbarJavascriptClientFixture() {
+            this._rollbarJsClient = new RollbarJavascriptClient();
+        }
+
+        [Fact]
+        public void Empty_request_rendered_as_empty_dict() {
+            Assert.Equal("{}", JsonConvert.SerializeObject(_rollbarJsClient));
+        }
+
+    }
+}
